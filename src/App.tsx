803bf9798e26cf416f4b0c7853f4f6529bafb3e3
Natalie <nataliembatha@gmail.com>
2023-05-20 20:47:52 +0200

    styling and added an error state

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ interface ApiData {
   _id: string;
   content: string;
   author: string;
-  tags: [];
+  tags: string [];
   authorSlug: string;
   length: number;
   dateAdded: string;
@@ -29,18 +29,19 @@ interface ApiData {
 const App = () => {
 
   const [quoteData, setQuoteData] = useState<ApiData[]>([])
+  const [error, setError] = useState<string>('')
 
   useEffect(() => {
     getQuote(BASEURL)
-  }, [BASEURL])
+  }, [])
   
   const getQuote = async (url: string) => {
     try {
       const response = await axios.get<ApiData[]>(url)
-      console.log(response.data)
+      // console.log(response.data)
       setQuoteData(response.data)
     } catch (error) {
-      console.error(error)
+      setError('Failed to fetch the quote, please try again...')
     }
   }
 
@@ -53,32 +54,45 @@ const App = () => {
       <NavBar />
       <div className="main-wrapper">
         <Container className="wrapper d-flex flex-column justify-content-between lh-sm">
-            <Row className="justify-content-start text-wrap">
-              {/* id-text - On first load, my quote machine displays a random quote in the element */}
-              {quoteData.length > 0 && quoteData[0]?.content}
-            </Row>
-            <Row className="justify-content-end fw-semibold fst-italic">
-              {/* id-author  - On first load, my quote machine displays the random quote's author */}
-              -- {quoteData.length > 0 && quoteData[0]?.author} --
-            </Row>
-            <Button 
-              // id-new-quote
-              className="text-uppercase fw-bold"
-              variant="outline-dark"
-              size="sm"
-              onClick={handleClick}
-            >
-              new quote <TbHandClick />
-            </Button>
-            <a 
-              // id tweet-quote
-              className="btn btn-outline-primary text-uppercase fw-bold" 
-              href="twitter.com/intent/tweet"
-              target="_blank"
-            >
-              {/* tweet the current tweet */}
-              tweet <FaRetweet />
-            </a>
+          {
+            error ?
+            ( 
+              <Row>
+                <p className="fs-2 fw-bolder">{error}</p>
+              </Row> 
+            )
+            :
+              (
+                <>
+                  <Row className="justify-content-start text-wrap">
+                    {/* id-text - On first load, my quote machine displays a random quote in the element */}
+                    {quoteData.length > 0 && quoteData[0]?.content }
+                  </Row>
+                  <Row className="justify-content-end fw-semibold fst-italic">
+                    {/* id-author  - On first load, my quote machine displays the random quote's author */}
+                    -- {quoteData.length > 0 && quoteData[0]?.author} --
+                  </Row>
+                  <Button 
+                    // id-new-quote
+                    className="text-uppercase fw-bold"
+                    variant="outline-dark"
+                    onClick={handleClick}
+                  >
+                    new quote <TbHandClick />
+                  </Button>
+                  <a 
+                    // id tweet-quote
+                    className="btn btn-outline-primary text-uppercase fw-bold" 
+                    href="twitter.com/intent/tweet"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {/* tweet the current tweet */}
+                    tweet <FaRetweet />
+                  </a>
+                </>
+              )
+          }
         </Container>
       </div>
     </>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,7 +4,7 @@ import Navbar from 'react-bootstrap/Navbar';
 const NavBar: React.FC = () => {
   return (
     <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#home" className="fs-2 m-2">
+        <Navbar.Brand href="#home" className="fs-1 ms-4">
           QuoteGenie
         </Navbar.Brand>
     </Navbar>
